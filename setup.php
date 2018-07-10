37bace8c71563405d06f59b37ca16cb3862407ae
Janosch Maier <janosch@crashtest-security.com>
2018-07-10 20:36:47 +0200

    feat: add output to setup file

--- setup.php
+++ setup.php
@@ -2,7 +2,9 @@
 
 include "connect.php";
 
+echo("Creating Database...");
+
 $query = file_get_contents("setup/create_db.sql");
 $result = $db->query($query);
 
-echo("Created Database.");
+echo($result);
