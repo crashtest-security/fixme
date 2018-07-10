4e353c6df2eddbd64dbe57fec63e9cec756ae729
Janosch Maier <janosch@crashtest-security.com>
2018-07-10 20:47:55 +0200

    feat: put sql query in setup.php file directly

--- setup.php
+++ setup.php
@@ -4,7 +4,8 @@ include "connect.php";
 
 echo("Creating Database...");
 
-$query = file_get_contents("setup/create_db.sql");
+$query = "DROP TABLE IF EXISTS `users`, `guestbook`; CREATE TABLE `users` (`id` int(11) NOT NULL AUTO_INCREMENT `username` varchar(255) NOT NULL, `password` varchar(255) NOT NULL,
+PRIMARY KEY(`id`)); CREATE TABLE `guestbook` (`id` int(11) NOT NULL AUTO_INCREMENT, `username` VARCHAR(255) NOT NULL, `entry` TEXT NOT NULL, PRIMARY KEY(`id`)); INSERT INTO `users` (`username`, `password`) VALUES ('admin', 'haXXor'), ('user', '12345678');";
 $result = $db->query($query);
 
 echo($result);
