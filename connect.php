224d8acd1bfa243269dcbf21c9c713f1d97aeb26
Daniel Schosser <daniel@crashtest-security.com>
2020-10-27 13:33:35 +0100

    feat: connect to heroku DB

--- connect.php
+++ connect.php
@@ -2,16 +2,18 @@
     // A simple PHP script demonstrating how to connect to MySQL.
     // Press the 'Run' button on the top to start the web server,
     // then click the URL that is emitted to the Output tab of the console.
-
-    $servername = "localhost";
-    $username = "che";
-    $password = "che";
-    $database = "che_db";
+​
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+​
+    $servername = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
     $dbport = 3306;
-
+​
     // Create connection
     $db = new mysqli($servername, $username, $password, $database, $dbport);
-
+​
     // Check connection
     if ($db->connect_error) {
         die("Connection failed: " . $db->connect_error);
